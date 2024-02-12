609e6696fd8da5037b9313b8f32916af1214e5d0
Aissatou85 <hguidado@gmail.com>
2024-02-12 21:00:53 +0100

    modification bucket_sort

--- TP3/bucket_sort.py
+++ TP3/bucket_sort.py
@@ -9,47 +9,37 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 
-#processus maitre
-def master_process(data):
+if rank == 0:
+    data = np.random.randint(0,100, size=20)
     min_value = min(data)
     max_value= max(data)
     bucket_size = (max_value - min_value) / (size-1)
     
-    #Envoie des buckets aux processus esclaves
+    #Envoie des buckets aux autres processus 
     for i in range(1,size):
         start = min_value + (i-1)*bucket_size
         end = min_value + i*bucket_size
         bucket_data = [x for x in data if start <= x < end]
         comm.send(bucket_data, dest=i)
         
-    #Reception des données triées des processus esclaves
+    #Reception des données triées des autres processus 
     sorted_data = []
     for i in range(1,size):
         sorted_bucket= comm.recv(source = i)
         sorted_data.extend(sorted_bucket)
+   
+    print("Le tableau Initial:", data)
+    print("Le tableau trie:", sorted_data)
     
-    return sorted_data
-
-def slave_process():
-    #reception des données du processus maître
+else:
+    #reception des données du processus 0
     bucket = comm.recv(source=0)
     
     #Tri local
     sorted_bucket = sorted(bucket)
     
-    #Envoi des données triées au processus maître
+    #Envoi des données triées au processus 0
     comm.send(sorted_bucket, dest=0)
-    
-
-if rank == 0:
-    #Génération du tableau de nombres arbitraires
-    data = np.random.randint(0,100, size=20)
-    sorted_data = master_process(data)
-    print("Le tableau Initial:", data)
-    print("Le tableau trié:", sorted_data)
-    
-else:
-    slave_process()
     
     
     
